adb441820651d51fcd1fecf40678acb270b1e79a
PimPAm012 <loreyna9090@gmail.com>
2023-07-25 06:31:49 -0300

    Primera parte - Estructura de la pagina

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(3456, () => {
+    console.log('Servidor corriendo');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
